8d41b5273fb92edb0a5839f57ba8d89de88ee46d
pedrovotf <pedrovotf@gmail.com>
2023-07-11 21:33:46 -0300

    realizado post para criação do usuário

--- lanchonete_FRONT/script/admin_home.js
+++ lanchonete_FRONT/script/admin_home.js
@@ -1,10 +1,7 @@
 
 const formulario = document.querySelector('#forms-login');
 const createUser = document.querySelector('#cadastro');
-
-
-
-
+const formularioCreateUser = document.querySelector('#enviarCadastro');
 
 
 formulario.addEventListener('submit', function loginUser(e){ //login
@@ -14,8 +11,6 @@ formulario.addEventListener('submit', function loginUser(e){ //login
     const password = document.forms['forms-login'].password.value;
     const mensagemErro = document.querySelector('#mensagemErro');
 
-    
-    
     if(!login || !password){
         let mensagemErroTexto = 'Ops, veifique se preencheu todos os campos'
         mensagemErro.innerHTML=mensagemErroTexto
@@ -53,12 +48,10 @@ formulario.addEventListener('submit', function loginUser(e){ //login
         
     }
     else{console.log('ERROR')}
-    
-
 })
  
 
-createUser.onclick = function(e){ //mudar classe do form
+createUser.onclick = function(e){ //mudar para form cadastro
     e.preventDefault();
 
     const introForm = document.querySelector('.intro')
@@ -71,4 +64,45 @@ createUser.onclick = function(e){ //mudar classe do form
     user_created.style.display = 'none'
     introForm.style.display = 'none'
 } 
+
+formularioCreateUser.onclick= function insertUser(e){//cadastro
+    e.preventDefault();
+
+    const name = document.forms['fromCadastrar'].cadastroNome.value;
+    const mail = document.forms['fromCadastrar'].cadastroMail.value;
+    const phone = document.forms['fromCadastrar'].cadastroTel.value;
+    const adress = document.forms['fromCadastrar'].cadastroAdress.value;
+    const password = document.forms['fromCadastrar'].cadastroPassword.value;
+    const mensagemRetorno = document.querySelector('#mensagemRetorno')
+    let text = ''
+
+    fetch(`http://localhost:8080/api//clientes`, {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                "name_cliente":name ,
+                mail,
+                "phone_number": phone,
+                adress,
+                password,
+            })})
+            .then(response =>{
+                response.json().
+                    then(data =>{
+                        if(data.message === 'Email ou telefone ja cadastrado'){
+                            text = data.message
+                            mensagemRetorno.innerHTML = text
+                        } else if(data.message === 'Sucesso'){
+                            text = 'Criado com sucesso'
+                            mensagemRetorno.innerHTML = text
+                        } else {
+                            text = 'Erro, gentileza tentar mais tarde'
+                            mensagemRetorno.innerHTML = text
+                        }
+                    })
+                })
+                .catch(err=>console.log(err))
+}
 
